5f2057a39803601bdb3bfbc0b69df483724ddf8c
terjevolke <terjevolke@gmail.co>
2022-01-28 09:48:45 +0200

    added fetch for production, consumption and solar energy for 1 day

--- app.js
+++ app.js
@@ -3,51 +3,116 @@ const y = today.getFullYear();
 const m = today.getMonth();
 const labels = [];
 const priceData = [];
+const productionData = [];
+const consumptionData = [];
+const solar_energy_productionData = [];
 const start = new Date(y, m, 0).toISOString();
 const end = new Date(y, m, 1).toISOString();
 //console.log(start);
 //console.log(end);
 
 fetch(`https://dashboard.elering.ee/api/nps/price?start=${start}&end=${end}`)
-  //fetch(`https://id.energia.ee/delegate/5g-api/v2/consumption?start-date=2021-01-01&end-date=2022-01-01&interval=HOUR&eic=38ZEE-00625607-9`)
 
-  .then((response) => response.json())
-  .then((res) => {
-    res.data.ee.forEach((el) => {
-      //console.log(res);
-      const date = new Date(el.timestamp * 1000);
-      const options = {
-        year: "numeric",
-        month: "long",
-        day: "2-digit",
-        hour: "2-digit",
-        second: "2-digit",
-      };
-      const localDate = date.toLocaleString("et-EE", options);
-      //console.log(localDate, el.price);
-      labels.push(localDate);
-      priceData.push(el.price);
+
+    .then((response) => response.json())
+    .then((res) => {
+        res.data.ee.forEach((el) => {
+            //console.log(res);
+            const date = new Date(el.timestamp * 1000);
+            const options = {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+                //  hour: "2-digit",
+                //  second: "2-digit",
+            };
+            const localDate = date.toLocaleString("et-EE", options);
+            //console.log(localDate, el.price);
+            labels.push(localDate);
+            priceData.push(el.price);
+        });
+        console.log(labels);
+        console.log(priceData);
+
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: "Hind",
+                    backgroundColor: "rgb(88, 156, 64)",
+                    borderColor: "rgb(88, 156, 64)",
+                    data: priceData,
+                },
+            ],
+        };
+
+        const config = {
+            type: "bar",
+            data: data,
+            options: {},
+        };
+
+        const myChart = new Chart(document.getElementById("myChart"), config);
     });
-    console.log(labels);
-    console.log(priceData);
 
-    const data = {
-      labels: labels,
-      datasets: [
-        {
-          label: "Hind",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
-          data: priceData,
-        },
-      ],
-    };
+fetch(`https://dashboard.elering.ee/api/system/with-plan?start=${start}&end=${end}`)
 
-    const config = {
-      type: "line",
-      data: data,
-      options: {},
-    };
+    .then((response) => response.json())
+    .then((res) => {
+        console.log(res);
+        res.data.real.forEach((el) => {
 
-    const myChart = new Chart(document.getElementById("myChart"), config);
-  });
+          const date = new Date(el.timestamp * 1000);
+            const options = {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+               hour: "2-digit",
+                //    second: "2-digit",
+            };
+            const localDate = date.toLocaleString("et-EE", options);
+            console.log(localDate, el.production);
+            labels.push(localDate);
+            productionData.push(el.production);
+            consumptionData.push(el.consumption);
+            solar_energy_productionData.push(el.solar_energy_production);
+        });
+        console.log(labels);
+
+        console.log(productionData);
+        console.log(consumptionData);
+        console.log(solar_energy_productionData);
+
+
+        const data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: "Tootmine",
+                    backgroundColor: "rgb(88, 156, 64)",
+                    borderColor: "rgb(88, 156, 64)",
+                    data: productionData,
+                },
+                {
+                     label: "Päike",
+                     backgroundColor: "rgb(235, 152, 52)",
+                     borderColor: "rgb(235, 152, 52)",
+                     data: solar_energy_productionData,
+                   },
+                   {
+                     label: "Tarbimine",
+                     backgroundColor: "rgb(156, 64, 70)",
+                     borderColor: "rgb(156, 64, 70)",
+                     data: consumptionData,
+                   },
+            ],
+        };
+
+        const config = {
+            type: "line",
+            data: data,
+            options: {},
+        };
+
+        const myChart1 = new Chart(document.getElementById("myChart1"), config);
+    });
